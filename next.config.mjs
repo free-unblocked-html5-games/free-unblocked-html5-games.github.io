063a1e5981734830fe147e263751936f2b4b7198
tupta9x <tupta1999@gmail.com>
2025-04-17 22:41:03 +0700

    updaate

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: "export",
+  basePath: "/free-unblocked-html5-games.github.io",
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
